e1df8c912ea24a4339043f0083793abb17edd858
joyguo0 <joyguo0@qq.com>
2014-01-26 13:47:06 +0800

    init

---
+++ mysite/_config.php
@@ -0,0 +1,17 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'root123',
+	"database" => 'ss_mysite',
+	"path" => '',
+);
+
+// Set the site locale
+i18n::set_locale('zh_CN');
